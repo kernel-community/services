58a10054bebfbb7ef439b28bcad1c1e199e8dc22
simon <simon@kernel.community>
2022-04-09 13:43:52 -0700

    cleanup

--- admin/src/index.js
+++ admin/src/index.js
@@ -8,9 +8,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'index.css';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
