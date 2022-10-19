5777c48fb8c78b29fe0962c39d7078c99bcb32d8
andytudhope <tuddy0525@gmail.com>
2022-10-19 11:06:15 +0200

    Updates application copy, adds one field, fixes greatful

--- packages/applications/src/components/Intro.jsx
+++ packages/applications/src/components/Intro.jsx
@@ -7,15 +7,16 @@
  */
 
 const Paragraph = ({ children }) =>
-  <p className='p-4'>{children}</p>
+  <p className='py-4'>{children}</p>
 
 const Intro = () => (
   <>
     <h1 className='py-4 text-4xl'>KERNEL Community Application</h1>
     <div className='columns-1 md:break-after-column'>
-      <Paragraph>This application is the start of your Kernel Unprofile.</Paragraph>
-      <Paragraph>What you write here will be visible to a group of current fellows who review applications.</Paragraph>
+      <Paragraph>This application is the start of your Kernel Unprofile. Rather than project what you think others want to hear, we ask that you speak the truth with love. This is what serves our core goal, which is to <strong>learn together</strong>.</Paragraph>
+      <Paragraph>What you write here will be visible to a group of current fellows who review applications. We value honesty, sincerity, simplicity and a diversity of skills and interests. <a className='text-blue-600 visited:text-purple-600' href='https://www.kernel.community/en/start/principled-people/' target='_blank' rel='noreferrer'>Our principles</a> are <strong>do no harm</strong> and <strong>play, infinitely</strong>. The purpose of this form is for us to get to know you. We will be most impressed by the quality of your intention, which we think is revealed through considerately crafted answers.</Paragraph>
       <Paragraph>If you are accepted, you’ll be able to update it continuously. It is your data and you are in control. This also means you are responsible for how it appears. The interplay of control and responsibility is one of the <a className='text-blue-600 visited:text-purple-600' href='https://www.kernel.community/en/learn/module-0/play-of-pattern/' target='_blank' rel='noreferrer'>complementary opposites</a> on which Kernel is based.</Paragraph>
+      <Paragraph>We save the fields in this form automatically, so feel free to take your time and don't worry if something seems to go wrong.</Paragraph>
     </div>
   </>
 )
